8c7c8a308b54f4177ab511144b9fe7565db27268
Deepak Rameshkumar <dr439@afsconnect2.njit.edu>
2017-02-27 14:21:23 -0500

    edit database

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=dr439';
+    $username = 'dr439';
+    $password = 'ebRbuPFN';
 
     try {
         $db = new PDO($dsn, $username, $password);
